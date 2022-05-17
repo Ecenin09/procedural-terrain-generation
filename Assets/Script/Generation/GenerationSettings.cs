9a9731ad8b18ea88b3a6e01b0682b9a4ae54cd3d
vladislav.telega <ecenin09@gmail.com>
2022-05-17 13:05:58 +0300

    [PTG-0] add: octave calculation

--- Assets/Script/Generation/GenerationSettings.cs
+++ Assets/Script/Generation/GenerationSettings.cs
@@ -8,11 +8,25 @@ namespace Script.Generation
     {
         [SerializeField] private Vector2Int _mapSize;
         [SerializeField] private float _scale;
+        [SerializeField] private int _octaves;
+        [Range(0,1)]
+        [SerializeField] private float _persistance;
+        [SerializeField] private float _lacunarity;
+        [SerializeField] private int _seed;
+        [SerializeField] private Vector2 _offset;
 
         [Header("Editor Option")] [SerializeField]
         public bool IsAutoUpdate = true;
 
-        public Vector2Int MapSize => _mapSize;
+        public Vector2Int MapSize
+        {
+            get
+            {
+                _mapSize.x = _mapSize.x < 1 ? 1 : _mapSize.x;
+                _mapSize.y = _mapSize.y < 1 ? 1 : _mapSize.y;
+                return _mapSize;
+            }
+        }
 
         public float Scale
         {
@@ -24,6 +38,26 @@ namespace Script.Generation
                 }
 
                 return _scale;
+            }
+        }
+
+        public int Octaves
+        {
+            get
+            {
+                return _octaves < 0 ? 0 : _octaves;
+            }
+        }
+
+        public float Persistance => _persistance;
+        public float Lacunarity => _lacunarity;
+        public int Seed => _seed;
+        public Vector2 Offset => _offset;
+        public Vector2 HalfMapSize
+        {
+            get
+            {
+                return new Vector2(_mapSize.x/2f,_mapSize.y/2f);
             }
         }
     }
--- Assets/Script/Generation/NoiseRealization/Noise.cs
+++ Assets/Script/Generation/NoiseRealization/Noise.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 using UnityEngine;
 
 namespace Script.Generation.NoiseRealization
@@ -8,22 +8,72 @@ namespace Script.Generation.NoiseRealization
         public static float[,] GenerateNoiseMap(GenerationSettings settings)
         {
             float[,] resultNoiseMap = new float[settings.MapSize.x, settings.MapSize.y];
-            float scale = settings.Scale;
+
+            Vector2[] octaveOffsets = new Vector2[settings.Octaves];
+            for (int i = 0; i < settings.Octaves; i++)
+            {
+                float offsetX = GetOffset(settings.Seed) + settings.Offset.x;
+                float offsetY = GetOffset(settings.Seed) + settings.Offset.y;
+
+                octaveOffsets[i] = new Vector2(offsetX, offsetY);
+            }
+
+            float maxNoiseHeight = float.MinValue;
+            float minNoiseHeight = float.MaxValue;
 
             for (int y = 0; y < settings.MapSize.y; y++)
             {
                 for (int x = 0; x < settings.MapSize.x; x++)
                 {
-                    float sampleX = x / scale;
-                    float sampleY = y / scale;
+                    float amplitude = 1;
+                    float frequency = 1;
+                    float noiseHeigth = 0;
 
-                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                    for (int i = 0; i < settings.Octaves; i++)
+                    {
+                        float sampleX = (x - settings.HalfMapSize.x) / settings.Scale * frequency + octaveOffsets[i].x;
+                        float sampleY = (y - settings.HalfMapSize.y) / settings.Scale * frequency + octaveOffsets[i].y;
 
-                    resultNoiseMap[x, y] = perlinValue;
+                        float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+
+                        noiseHeigth += perlinValue * amplitude;
+
+                        amplitude *= settings.Persistance;
+                        frequency *= settings.Lacunarity;
+
+                        resultNoiseMap[x, y] = perlinValue;
+                    }
+
+                    if (noiseHeigth > maxNoiseHeight)
+                    {
+                        maxNoiseHeight = noiseHeigth;
+                    }
+                    else if (noiseHeigth < minNoiseHeight)
+                    {
+                        minNoiseHeight = noiseHeigth;
+                    }
+
+                    resultNoiseMap[x, y] = noiseHeigth;
+                }
+            }
+
+            for (int y = 0; y < settings.MapSize.y; y++)
+            {
+                for (int x = 0; x < settings.MapSize.x; x++)
+                {
+                    resultNoiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, resultNoiseMap[x, y]);
                 }
             }
 
             return resultNoiseMap;
+        }
+
+
+        private static float GetOffset(int seed)
+        {
+            System.Random random = new System.Random(seed);
+
+            return random.Next(-10000, 10000);
         }
     }
 }
