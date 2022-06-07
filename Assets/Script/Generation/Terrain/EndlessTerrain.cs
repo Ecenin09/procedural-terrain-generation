7aa2733a7935447a49e26c245710f28a79866ab3
ecenin09 <ecenin09@gmail.com>
2022-06-07 14:31:47 +0300

    [PTG-0] add: generated endless terrain

--- Assets/Script/Generation/Terrain/EndlessTerrain.cs
+++ Assets/Script/Generation/Terrain/EndlessTerrain.cs
@@ -1,17 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using Script.Generation.Terrain.TerrainData;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Script.Generation.Terrain
 {
     public class EndlessTerrain : MonoBehaviour
     {
-        public const float MAX_VIEW_DISTANCE = 300f;
+        public const float MAX_VIEW_DISTANCE = 250f;
 
-        [SerializeField] private Transform viewer;
+        [SerializeField] private Transform _viewer;
+        [SerializeField] private Transform _chunkParent;
 
         private Vector2 _viwerPosition;
         private int _chunkSize;
         private int _chunkVisibleInViewDistance;
+
+        private Dictionary<Vector2, TerrainChunk> _terrainChunks = new Dictionary<Vector2, TerrainChunk>();
+        private List<TerrainChunk> _terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
 
         private void Start()
         {
@@ -19,8 +26,16 @@ namespace Script.Generation.Terrain
             _chunkVisibleInViewDistance = Mathf.RoundToInt(MAX_VIEW_DISTANCE / _chunkSize);
         }
 
+        private void Update()
+        {
+            _viwerPosition = new Vector2(_viewer.position.x, _viewer.position.z);
+            UpdateVisibleChunk();
+        }
+
         private void UpdateVisibleChunk()
         {
+            DisableLastVisibleChunks();
+
             Vector2Int currentChunkPosition = new Vector2Int(Mathf.RoundToInt(_viwerPosition.x / _chunkSize),
                 Mathf.RoundToInt(_viwerPosition.y / _chunkSize));
 
@@ -30,7 +45,38 @@ namespace Script.Generation.Terrain
                 {
                     Vector2 viewedChunkCoord =
                         new Vector2(currentChunkPosition.x + xOffset, currentChunkPosition.y + yOffset);
+                    ShowTerrainChunk(viewedChunkCoord, ref _terrainChunks);
                 }
+            }
+        }
+
+        private void ShowTerrainChunk(Vector2 viewedChunkCoord, ref Dictionary<Vector2, TerrainChunk> terrainChunks)
+        {
+            if (terrainChunks.ContainsKey(viewedChunkCoord))
+            {
+                var chunk = terrainChunks[viewedChunkCoord].UpdateTerrainChunk(_viwerPosition, MAX_VIEW_DISTANCE);
+
+                if (chunk.IsVisible)
+                {
+                    _terrainChunksVisibleLastUpdate.Add(chunk);
+                }
+            }
+            else
+            {
+                terrainChunks.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, _chunkSize, _chunkParent));
+            }
+        }
+
+        private void DisableLastVisibleChunks()
+        {
+            if (_terrainChunksVisibleLastUpdate != null)
+            {
+                foreach (var chunk in _terrainChunksVisibleLastUpdate)
+                {
+                    chunk.SetVisible(false);
+                }
+
+                _terrainChunksVisibleLastUpdate.Clear();
             }
         }
     }
---
+++ Assets/Script/Generation/Terrain/TerrainData/TerrainChunk.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Script.Generation.Terrain.TerrainData
+{
+    public class TerrainChunk
+    {
+        private Vector2 _position;
+        private GameObject _meshObject;
+        private Bounds _bounds;
+
+        public bool IsVisible => _meshObject.activeSelf;
+        public TerrainChunk(Vector2 coord, int size, Transform parent)
+        {
+            _position = coord * size;
+            _bounds = new Bounds(_position, Vector3.one * size);
+            var localMeshScale = Vector3.one * size;
+            
+            Vector3 positionInSpace = new Vector3(_position.x, 0, _position.y);
+            
+            _bounds = new Bounds(_position, localMeshScale);
+            _meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            _meshObject.transform.SetParent(parent);
+            _meshObject.transform.position = positionInSpace;
+            _meshObject.transform.localScale = localMeshScale / 10f;
+        }
+
+        public TerrainChunk UpdateTerrainChunk(Vector2 viewerPosition, float maxViewDistance)
+        {
+            float viewerDistanceNearestEdge = _bounds.SqrDistance(viewerPosition);
+
+            bool isVisible = viewerDistanceNearestEdge <= Mathf.Pow(maxViewDistance ,2f);
+
+            SetVisible(isVisible);
+
+            return this;
+        }
+
+        public void SetVisible(bool state)
+        {
+            _meshObject.SetActive(state);
+        }
+    }
+}
